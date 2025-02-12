239516c467b3602bf946195ca7cf1c40238d820b
daniel <d260404@dac.unicamp.br>
2025-02-12 16:12:18 -0300

    feat: Initial Config

---
+++ src/test/java/com/dnss/api_ti9/ApiTi9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnss.api_ti9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTi9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
