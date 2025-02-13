f714f425f38f9f2e1dde1b4b6c13de0d4a7ee4c9
daniel <d260404@dac.unicamp.br>
2025-02-13 12:57:37 -0300

    feat: Injecao de dependencia do repository em FuncionarioService

--- src/main/java/com/dnss/api_ti9/service/FuncionarioService.java
+++ src/main/java/com/dnss/api_ti9/service/FuncionarioService.java
@@ -1,4 +1,12 @@
 package com.dnss.api_ti9.service;
 
+import com.dnss.api_ti9.repository.FuncionarioRepository;
+
 public class FuncionarioService {
+
+    private FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
 }
