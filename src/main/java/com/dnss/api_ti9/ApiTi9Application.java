1f88cbff6f6b52f6b9564e804d7dd157a77313fe
daniel <d260404@dac.unicamp.br>
2025-02-16 18:36:46 -0300

    feat: main

---
+++ src/main/java/com/dnss/api_ti9/ApiTi9Application.java
@@ -0,0 +1,13 @@
+package com.dnss.api_ti9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTi9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTi9Application.class, args);
+	}
+
+}
