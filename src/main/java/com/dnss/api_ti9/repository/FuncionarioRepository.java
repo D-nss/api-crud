92c841c5373448a0366414196ac37aa2853fb123
daniel <d260404@dac.unicamp.br>
2025-02-17 19:51:08 -0300

    feat: findByCpf()

--- src/main/java/com/dnss/api_ti9/repository/FuncionarioRepository.java
+++ src/main/java/com/dnss/api_ti9/repository/FuncionarioRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.dnss.api_ti9.model.Funcionario;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, UUID> {
+    Optional<Funcionario> findByCpf(String cpf);
 }
