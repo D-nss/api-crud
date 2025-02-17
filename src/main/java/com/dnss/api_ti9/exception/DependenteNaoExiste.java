03d20bc33cef311f08d72bb56fb9f3aeb111c01a
daniel <d260404@dac.unicamp.br>
2025-02-17 12:31:06 -0300

    feat: Exception dependente nao existe

---
+++ src/main/java/com/dnss/api_ti9/exception/DependenteNaoExiste.java
@@ -0,0 +1,7 @@
+package com.dnss.api_ti9.exception;
+
+public class DependenteNaoExiste extends RuntimeException {
+    public DependenteNaoExiste(String message) {
+        super(message);
+    }
+}
