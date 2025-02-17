e3072b12dbc748ec3c5b018262647ac96d74585b
daniel <d260404@dac.unicamp.br>
2025-02-17 19:51:26 -0300

    feat: FuncionarioResponseDTO

--- src/main/java/com/dnss/api_ti9/dto/FuncionarioResponseDTO.java
+++ src/main/java/com/dnss/api_ti9/dto/FuncionarioResponseDTO.java
@@ -1,4 +1,15 @@
 package com.dnss.api_ti9.dto;
 
-public record FuncionarioResponseDTO() {
+import com.dnss.api_ti9.model.Dependente;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public record FuncionarioResponseDTO(String nome,
+                                     String cpf,
+                                     String cargo,
+                                     BigDecimal salario,
+                                     LocalDate data_de_admissao,
+                                     List<Dependente> dependentes) {
 }
