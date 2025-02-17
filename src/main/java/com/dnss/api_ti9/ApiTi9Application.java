6f8b70acab4700f6584ab29112b4030c85b86926
daniel <d260404@dac.unicamp.br>
2025-02-17 19:52:52 -0300

    feat: swagger

--- src/main/java/com/dnss/api_ti9/ApiTi9Application.java
+++ src/main/java/com/dnss/api_ti9/ApiTi9Application.java
@@ -1,9 +1,12 @@
 package com.dnss.api_ti9;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title ="Swagger OpenApi", version ="'", description = "API desenvolvida para testes do OpenApi"))
 public class ApiTi9Application {
 
 	public static void main(String[] args) {
