902c7da554ccad4309b585e2184f6b645a910243
daniel <d260404@dac.unicamp.br>
2025-02-17 12:31:20 -0300

    feat: Exception data admissao invalida

---
+++ src/main/java/com/dnss/api_ti9/exception/InvalidoDataAdmissao.java
@@ -0,0 +1,7 @@
+package com.dnss.api_ti9.exception;
+
+public class InvalidoDataAdmissao extends RuntimeException {
+    public InvalidoDataAdmissao(String message) {
+        super(message);
+    }
+}
