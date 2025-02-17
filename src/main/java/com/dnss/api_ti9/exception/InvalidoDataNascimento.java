f1a1877be2b2b333fdd2e881ef3a5b249677b0b0
daniel <d260404@dac.unicamp.br>
2025-02-17 12:31:31 -0300

    feat: Exception data nascimento invalida

---
+++ src/main/java/com/dnss/api_ti9/exception/InvalidoDataNascimento.java
@@ -0,0 +1,7 @@
+package com.dnss.api_ti9.exception;
+
+public class InvalidoDataNascimento extends RuntimeException {
+    public InvalidoDataNascimento(String message) {
+        super(message);
+    }
+}
